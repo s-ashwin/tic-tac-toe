526df9563e8667679aa22bfe03000f9ff942cdc0
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-07-28 13:00:31 +0530

    Updates

---
+++ precache-manifest.89829f0f9fc8d13ac08f773e0d0cac28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15a34636cd655e22f55ad41c12f225d",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "9831d87099f08ad0adc5",
+    "url": "/tic-tac-toe/static/css/2.52f28872.chunk.css"
+  },
+  {
+    "revision": "13fb35847f3bf5423ee7",
+    "url": "/tic-tac-toe/static/css/main.42804fed.chunk.css"
+  },
+  {
+    "revision": "9831d87099f08ad0adc5",
+    "url": "/tic-tac-toe/static/js/2.e5599a1b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.e5599a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13fb35847f3bf5423ee7",
+    "url": "/tic-tac-toe/static/js/main.2cf4c59a.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
